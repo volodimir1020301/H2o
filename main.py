19ea1d131658ef71d13fb6265b45a5d5fd9adb5d
Vislyy <romanpavelko70@gmail.com>
2025-07-09 12:29:24 +0300

    qwer
    qwr

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import  random
 WIDTH = 1000
 HEIGHT = 600
+
 class Player:
     def __init__(self, x, y, size, color, speed,name ):
         self.hitbox = pygame.Rect(x, y, size, size)
@@ -10,13 +11,23 @@ class Player:
         self.name = name
         self.x = x
         self.y = y
-    def draw(self, window):
+        self.size = size
+    def draw(self, window, scale=1):
+        draw_x = int(WIDTH // 2 - (self.size * scale) //2)
+        draw_y = int(HEIGHT // 2 - (self.size * scale) // 2)
+        self.hitbox = pygame.Rect(draw_x, draw_y, self.size * scale, self.size * scale)
         pygame.draw.rect(window, self.color, self.hitbox)
 
     def update(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d]:
             self.x += self.speed
+        if keys[pygame.K_s]:
+            self.y += self.speed
+        if keys[pygame.K_a]:
+            self.x -= self.speed
+        if keys[pygame.K_w]:
+            self.y -= self.speed
 
 
 class Food:
@@ -25,10 +36,16 @@ class Food:
         self.color = color
         self.x = x
         self.y = y
-    def draw(self, window):
+        self.size = size
+    def draw(self, window, player_x, player_y, scale):
+        draw_x = int((self.x - player_x) * scale + WIDTH // 2)
+        draw_y = int((self.y - player_y) * scale + HEIGHT // 2)
+        self.hitbox = pygame.Rect(draw_x, draw_y, self.size * scale, self.size * scale)
         pygame.draw.rect(window, self.color, self.hitbox)
 pygame.init()
-
+pygame.display.set_caption("Надпис на екрані")
+fon = pygame.font. SysFont("ARial",36 )
+text = fon.render("очки: ", True, (0, 0, 0))
 window = pygame.display.set_mode([WIDTH, HEIGHT])
 
 clock = pygame.time.Clock()
@@ -39,18 +56,27 @@ for i in range(300):
                  random.randint(-2000, 2000),
                  [random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)])
     foods.append(food)
-andriy = Player(WIDTH/2, HEIGHT/2, 50, [255, 0, 0], 3, "любомир")
+    scale = 1
+andriy = Player(WIDTH/2, HEIGHT/2, 50, [255, 0, 0], 8, "любомир")
+
 while 1==1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    scale = max(0.3, min(60 / andriy.size, 1.5))
+    for food in foods:
+        if andriy.hitbox.colliderect(food.hitbox):
+            food.x = random.randint(-3000, 3000)
+            food.y = random.randint(-3000, 3000)
+            andriy.size += 2
     window.fill([255, 255, 255])
-    andriy.draw(window)
+    andriy.draw(window, scale)
+    window.blit(text, (0, 0))
     andriy.update()
 
     for food in foods:
-        food.draw(window)
+        food.draw(window, andriy.x, andriy.y, scale)
         
     pygame.display.flip()
 
