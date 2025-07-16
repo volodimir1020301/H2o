781c2c455c88ab67876a492244cc2f94039fe005
Vislyy <147308738+volodimir1020301@users.noreply.github.com>
2025-07-16 12:24:09 +0300

    ytu
    ytur

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import json
+import socket
+import threading
+import time
+
 import pygame
 import  random
 WIDTH = 1000
 HEIGHT = 600
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("2.tcp.eu.ngrok.io", 15132))
 
+player_id = client.recv(1024). decode()
+other_players_data = {}
+
+def receiv_data():
+    global  other_players_data
+    while True:
+        try:
+            data = client.recv(4096).decode()
+            if data:
+                other_players_data = json.loads(data)
+        except:
+            break
+threading.Thread(target=receiv_data, daemon=True).start()
 class Player:
     def __init__(self, x, y, size, color, speed,name ):
         self.hitbox = pygame.Rect(x, y, size, size)
@@ -45,20 +65,20 @@ class Food:
 pygame.init()
 pygame.display.set_caption("Надпис на екрані")
 fon = pygame.font. SysFont("ARial",36 )
-text = fon.render("очки: ", True, (0, 0, 0))
+text = fon.render("H2o ", True, (0, 0, 0))
 window = pygame.display.set_mode([WIDTH, HEIGHT])
 
 clock = pygame.time.Clock()
 foods = [ ]
-for i in range(300):
+for i in range(400):
     food = Food(20,
                  random.randint(-2000, 2000),
                  random.randint(-2000, 2000),
                  [random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)])
     foods.append(food)
     scale = 1
-andriy = Player(WIDTH/2, HEIGHT/2, 50, [255, 0, 0], 8, "любомир")
-
+andriy = Player(WIDTH/2, HEIGHT/2, 50, [0, 0, 0], 5, "любомир")
+last_send_time = 0
 while 1==1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,13 +87,35 @@ while 1==1:
     scale = max(0.3, min(60 / andriy.size, 1.5))
     for food in foods:
         if andriy.hitbox.colliderect(food.hitbox):
-            food.x = random.randint(-3000, 3000)
-            food.y = random.randint(-3000, 3000)
+            food.x = random.randint(-2000, 2000)
+            food.y = random.randint(-2000, 2000)
             andriy.size += 2
     window.fill([255, 255, 255])
     andriy.draw(window, scale)
     window.blit(text, (0, 0))
     andriy.update()
+    if time.time() - last_send_time > 0.1 :
+        try:
+            players_data = json.dumps({
+                "x" : andriy.x,
+                "y" : andriy.y,
+                "size" : andriy.size,
+                "color" : andriy.color,
+                "name" : andriy.name,
+                "speed": andriy.speed
+            })
+            client.send(players_data.encode())
+        except Exception as e:
+            print("Connection lost:", e)
+            running = False
+        pass
+
+    for pid, pdata in other_players_data.items():
+        draw_x = int((pdata["x"] - andriy.x) * scale + WIDTH // 2 )
+        draw_y = int((pdata["y"] - andriy.y) * scale + HEIGHT // 2)
+        size = pdata["size"] * scale
+        pygame.draw.rect(window, pdata["color"], pygame.Rect(draw_x, draw_y, size, size))
+
 
     for food in foods:
         food.draw(window, andriy.x, andriy.y, scale)
